97703f00ffc32a3b54cab5016fbaaebc87df2214
Alejandra Tobar <alejandratcag@gmail.com>
2024-04-09 11:56:46 -0400

    modificaciones carrito

--- src/views/Carrito.jsx
+++ src/views/Carrito.jsx
@@ -1,40 +1,67 @@
-import { Link } from "react-router-dom";
-import { useContext } from "react";
-import { PizzasContext } from "../context/MyContext"; 
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { PizzasContext } from '../context/MyContext';
+import { Container, ListGroup, Button, Row, Col, Badge, Image } from 'react-bootstrap';
 
-const Navbar = () => {
-  const { carrito, getCarritoTotal } = useContext(PizzasContext); // Usando 'carrito' para contar los items
+const Carrito = () => {
+  const { carrito, getCarritoTotal, removeFromCarrito, addToCarrito } = useContext(PizzasContext);
+  const navigate = useNavigate();
 
-  // Contando los items en el carrito
-  const itemsCount = carrito.reduce((total, item) => total + item.quantity, 0);
-
-  // Llamando a getCarritoTotal para obtener el total del carrito
   const total = getCarritoTotal();
 
-  return (
-    <nav className="navbar bg-info">
-      {/* Brand */}
-      <Link to="/" className="navbar-brand text-white">
-        🍕 Pizzería Mamma Mia!
-      </Link>
+  const handleDecreaseQuantity = (pizzaId) => {
+    removeFromCarrito(pizzaId);
+  };
 
-      {/* Cart */}
-      <Link to="/carrito" className="navbar-cart text-white">
-        <span className="cart-icon">🛒</span>
-        {/* Mostrando el número de items en el carrito si hay más de cero */}
-        {itemsCount > 0 && (
-          <span className="cart-amount-badge">{itemsCount}</span>
-        )}
-        <span className="cart-amount">
-          {new Intl.NumberFormat("es-CL", {
-            style: "currency",
-            currency: "CLP",
-            minimumFractionDigits: 0,
-          }).format(total)}
-        </span>
-      </Link>
-    </nav>
+  const handleIncreaseQuantity = (pizzaId) => {
+    addToCarrito(pizzaId);
+  };
+
+  const handleCheckout = () => {
+    navigate('/pago');
+  };
+
+  return (
+    <Container>
+      <h2 className="text-center mt-4">Detalles del pedido:</h2>
+      <ListGroup className="my-3">
+        {carrito.map((item) => (
+          <ListGroup.Item key={item.id} className="d-flex justify-content-between align-items-center list-group-item-custom">
+            <Image src={item.img} width={64} height={64} rounded />
+            <div className="flex-grow-1 mx-3">{item.name}</div>
+            <div className="flex-shrink-0 mx-3 price">{new Intl.NumberFormat("es-CL", {
+              style: "currency",
+              currency: "CLP",
+            }).format(item.price)}
+            </div>
+            <div className="flex-shrink-0 d-flex align-items-center">
+              <Button variant="outline-danger" size="sm" onClick={() => handleDecreaseQuantity(item.id)} className="btn-quantity">
+                –
+              </Button>
+              <Badge bg="secondary" className="mx-2 badge-quantity">{item.quantity}</Badge>
+              <Button variant="outline-primary" size="sm" onClick={() => handleIncreaseQuantity(item.id)} className="btn-quantity">
+                +
+              </Button>
+            </div>
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+      <Row className="justify-content-end total-row">
+        <Col xs={12} md={6}>
+          <div className="total-price">
+            <span className="text-total">Total:</span> 
+            <span className="amount">{new Intl.NumberFormat("es-CL", {
+              style: "currency",
+              currency: "CLP",
+            }).format(total)}</span>
+          </div>
+          <Button variant="primary" size="lg" onClick={handleCheckout} className="mt-3 btn-checkout">
+            Ir a Pagar
+          </Button>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
-export default Navbar;
+export default Carrito;
